a61317ecd7635491f8b4f389b122ead6d8af4a31
SiyabongaMasuku <matupa.masuku@gmail.com>
2019-05-14 13:40:53 +0200

    powerF base (type: double and integers), exponent (only integers)

---
+++ Power.java
@@ -0,0 +1,26 @@
+import java.math.*;
+
+/* double base, exponent; //inputs
+ * 
+ */
+public class Power {
+	
+	 static double powerF(double a, int b){
+		double product = 1;
+		for (int i=1; i<= b; i++){
+			product = product  * a;			
+		}
+		return product;
+	}
+	 
+	
+	public static void main(String[] args){
+
+                //Compare powerF() to Math.pow()		
+		System.out.println("powerF(): "+powerF(2.5, 3));
+		System.out.println("Math.pow(): "+Math.pow(2.5, 3));
+		
+	}	
+
+}
+
