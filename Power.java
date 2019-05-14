fe3c215f5876ee93ad0e6202ad30793d380727ac
Siyabonga Masuku <matupa.masuku@gmail.com>
2019-05-14 13:54:48 +0200

    Update Power.java

--- Power.java
+++ Power.java
@@ -3,8 +3,7 @@ import java.math.*;
 /* double base, exponent; //inputs
  * 
  */
-public class Power {
-	
+public class Power {	
 	 static double powerF(double a, int b){
 		double product = 1;
 		for (int i=1; i<= b; i++){
@@ -13,14 +12,11 @@ public class Power {
 		return product;
 	}
 	 
-	
 	public static void main(String[] args){
 
                 //Compare powerF() to Math.pow()		
 		System.out.println("powerF(): "+powerF(2.5, 3));
-		System.out.println("Math.pow(): "+Math.pow(2.5, 3));
-		
+		System.out.println("Math.pow(): "+Math.pow(2.5, 3));		
 	}	
-
 }
 
